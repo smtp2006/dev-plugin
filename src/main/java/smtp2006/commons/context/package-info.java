38ccacd8cbdfaf1809a62532434435385d29d0da
王华(061021) <hua.wanghuawh@alibaba-inc.com>
2014-07-12 10:59:23 +0800

    add
    Signed-off-by: 王华(061021) <hua.wanghuawh@alibaba-inc.com>

--- src/main/java/smtp2006/commons/context/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2013.
- */
-/**
- * @author 王华
- * @version 2013年10月31日 上午11:19:48
- *
- */
-package smtp2006.commons.context;
--- src/main/java/smtp2006/commons/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2013.
- */
-/**
- * @author 王华
- * @version 2013年10月29日 下午5:11:36
- *
- */
-package smtp2006.commons;
